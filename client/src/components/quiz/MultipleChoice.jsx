ed0eb0ce4a4da869aecc63830ad14e2daf71f446
Djkarmur <dipakkarmur45@gmail.com>
2025-01-06 19:59:05 +0530

    [changes]

--- client/src/components/quiz/MultipleChoice.jsx
+++ client/src/components/quiz/MultipleChoice.jsx
@@ -460,7 +460,7 @@ const handleOptionClick = async (option) => {
       >
         <div
           className="card shadow p-4 "
-          style={{ width: "60vw", borderRadius: "15px" }}
+          style={{ width: "90vw", borderRadius: "15px" }}
         >
           <div className="card-body " style={{userSelect: "none"}}>
             <div className="d-flex justify-content-between items-center" >
@@ -485,7 +485,7 @@ const handleOptionClick = async (option) => {
                 </CountdownCircleTimer>
               ) : null} */}
 
-              <div className="card-title gap-2">
+              <div className="card-title gap-2 d-flex align-items-center">
               <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -502,6 +502,7 @@ const handleOptionClick = async (option) => {
                 >
                   <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12 12 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A20 20 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a20 20 0 0 0 1.349-.476l.019-.007.004-.002h.001" />
                 </svg>
+                <div className='d-flex '>
                 <button
                   className="btn btn-success px-3 py-2 w-auto text-18"
                   onClick={handleCancel}
@@ -515,6 +516,8 @@ const handleOptionClick = async (option) => {
                 >
                   Finish
                 </button>
+                </div>
+                
                 {(totalReviewed > 0 || skippedQuestion > 0) &&
                 remainingTimeRef.current !== 0 ? (
                   <Modal open={open} onClose={onCloseModal} center>
@@ -546,7 +549,14 @@ const handleOptionClick = async (option) => {
               </div>
             </div>
             <hr />
-            <h5 className="card-text text-center">{question}</h5>
+            <div
+              className="d-flex gap-4 justify-center "
+              style={{ alignItems: "center" }}
+            >
+              <h5 className="card-text text-center">{question}</h5>
+
+              <CommentForQuestion questionId={questionId} />
+            </div>
             {/* <ul className="list-group list-group-flush mt-3 mb-4 ">
               {options?.map((option, id) => (
                 <li
--- client/src/components/quiz/SingleChoice.jsx
+++ client/src/components/quiz/SingleChoice.jsx
@@ -395,10 +395,10 @@ const SingleChoice = ({
       >
         <div
           className="card shadow p-4 "
-          style={{ width: "60vw", borderRadius: "15px", userSelect: "none" }}
+          style={{ width: "90vw", borderRadius: "15px", userSelect: "none" }}
         >
           <div className="card-body ">
-            <div className="d-flex justify-content-between items-center">
+            <div className="d-flex justify-content-between items-center flex-wrap">
               <h4 className="card-title text-center">
                 Question {index} of {totalQuestions}{" "}
               </h4>
@@ -420,7 +420,7 @@ const SingleChoice = ({
                 </CountdownCircleTimer>
               ) : null} */}
 
-              <div className="card-title gap-2">
+              <div className="card-title gap-2 d-flex align-items-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -443,6 +443,7 @@ const SingleChoice = ({
                   <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12 12 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A20 20 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a20 20 0 0 0 1.349-.476l.019-.007.004-.002h.001" />
                 </svg>
                 {/* <i className="icon-flag pointer" aria-hidden="true" style={{fontSize:'25px',marginRight:'70px',marginTop:'50px',color:'red', backgroundColor:'blue'}}></i> */}
+                <div className='d-flex '>
                 <button
                   className="btn btn-success px-3 py-2 w-auto text-18"
                   onClick={handleCancel}
@@ -456,6 +457,8 @@ const SingleChoice = ({
                 >
                   Finish
                 </button>
+                </div>
+                
                 {(totalReviewed > 0 || skippedQuestion > 0) &&
                 remainingTimeRef.current !== 0 ? (
                   <Modal open={open} onClose={onCloseModal} center>
